1c1e8caa398c039d34c1fff38e57facc79d07c91
Kaveesha Hettige <2021is046@stu.ucsc.cmb.ac.lk>
2024-08-05 12:01:55 +0530

    developed basic getters and setters

--- src/Account.java
+++ src/Account.java
@@ -1,27 +1,34 @@
 
 public class Account {
+
+	private int accountNumber;
+	private int pinNumber;
+	private double checkingBalance = 0;
+	private double savingBalance = 0;
 	
 	public void setAccNum(int accnum) {
-		
+		this.accountNumber=accnum;
 	}
 	
 	public void setPinNum(int pinnum) {
+		this.pinNumber=pinnum;
 		
 	}
 	
 	public int getAccNum() {
-		return 1;
+		return this.accountNumber;
 	}
 	
 	public int getPinNum() {
-		return 1;	
+		return this.pinNumber;	
 	}
 	
 	public void viewBalanceC() {
-		
+		System.out.println("Checking Account Balance: " + checkingBalance);
 	}
 	
 	public void withdrawMoneyC() {
+		
 		
 	}
 	
@@ -30,7 +37,7 @@ public class Account {
 	}
 	
 	public void viewBalanceS() {
-		
+		System.out.println("Savings Account Balance: " + savingBalance);
 	}
 	
 	public void withdrawMoneyS() {
